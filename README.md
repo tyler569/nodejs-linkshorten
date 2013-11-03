92d9ca9d315eed57593f33ba03f17dc2ba9b140a
Tyler Philbrick <tjp569@gmail.com>
2013-11-02 20:29:09 -0400

    Converted to MySQL

--- README.md
+++ README.md
@@ -4,28 +4,25 @@ Node.JS-Link-Generator
 <h4>A link generator made in Node.JS using RethinkDB as a backend</h4>
 
 <h2><b>Install</b></h2>
-Update settings.json with the IP of the machine Node.js is running on and a ip of a RethinkDB cluster
+Update settings.json with the IP of the machine Node.js is running on and a ip of a MySQL (or equivalent) server
 
-Run: <code>npm install rethinkdb</code> <code>npm install url</code> to install the dependencies
+Update the SQL connection info in index.js to match your MySQL server's password and database name
 
-Then start the server with <code>node index.js</code>
+Run: <code>npm install mysql</code> <code>npm install url</code> to install the dependencies
+
+Then start the server with <code>nodejs index.js</code>
 <h2><b>How to use</b></h2>
-To generate a link vist <code>http://HOST/url?=</code> and fill in the url.
+To generate a link vist <code>http://HOST/shotrn/?url=</code> and fill in the url.
 
 <b>Example</b>
 
-<code>http://HOST/shorten?=http://google.ca</code>
+<code>http://HOST/shorten/?url=http://google.com</code>
 
-Will in this case generate a link with the id <code>291bc643-ad2a-46ea-a211-8d73db1c496f</code>
+Will in this case generate a link with the id <code>2</code>
 
 I can then use this to retrieve the url associated with that id.
 
-<code>http://HOST/url?=291bc643-ad2a-46ea-a211-8d73db1c496f</code>
+<code>http://HOST/url/?id=2</code>
 
-Will return with <code>http://google.ca</code>
+Will return with <code>http://google.com</code>
 
-
-<h2><b>Plans</b></h2>
-<ul>
-<li>Make the links shorter</li>
-</ul>
--- index.js
+++ index.js
@@ -1,61 +1,62 @@
 var http = require('http');
 var url = require('url');
-var r = require('rethinkdb')
+var mysql = require('mysql');
 var settings = require('./settings.json');
-r.connect({host: settings.rethinkdb_ip}, function(err, conn){
-	http.createServer(function (req, ress) {
-		if(req.url === '/favicon.ico'){
-			return;
+
+sql = mysql.createConnection({
+	"host": "localhost",
+	"user": "root",
+	"password": "root",
+	"database": "URL_Shortening"
+});
+sql.connect(function(err) {
+	if (err) throw err;
+})
+sql.query('CREATE TABLE IF NOT EXISTS urls (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, url VARCHAR(100));')
+
+http.createServer(function (req, ress) {
+	if (req.url === '/favicon.ico') {
+		return;
+	}
+	var parsedURL = url.parse(req.url)
+	if (parsedURL['pathname'] === '/shorten'){
+		var parsedUrl = url.parse(req.url, true);
+		var queryAsObject = parsedUrl.query;
+		var getURL = queryAsObject['url'];
+
+		if (typeof getURL != 'undefined') {
+			sql.query('INSERT INTO urls VALUES (id, ?);', [getURL], function(err, result){
+				if (err) throw err;
+				ress.writeHead(200, {'Content-Type': 'text/plain'})
+				ress.end('Your key is: '+result.insertId.toString());
+			});
+
+		} else {
+			ress.writeHead(200, {'Content-Type': 'text/plain'});
+			ress.end("Please insure you have included a URL");
 		}
-		var parsedURL = url.parse(req.url)
-		if(parsedURL['pathname'] === '/shorten'){
-			var parsedUrl = url.parse(req.url, true);
-			var queryAsObject = parsedUrl.query;
-			var getURL = queryAsObject['url'];
 
-			if (typeof getURL != 'undefined') {
+	} else if(parsedURL['pathname'] === '/url') {
 
-
-				r.db('URL_Shortening').table('urls').insert({'url':queryAsObject['url']}).run(conn, function(err, res) {
-					if(err) throw err;
-					id = res['generated_keys'];
-					console.log(id['0']);
-					ress.writeHead(200, {'Content-Type': 'text/plain'});
-					ress.end(id['0']);
-				});
-
-			}else{
+		var parsedUrl = url.parse(req.url, true);
+		var queryAsObject = parsedUrl.query;
+		var id = queryAsObject['id'];
+		if (typeof id != 'undefined') {
+			sql.query('SELECT url FROM urls WHERE id=?;', [id], function(err, rows){
+				if (err) throw err;
 				ress.writeHead(200, {'Content-Type': 'text/plain'});
-				ress.end("Please insure you have included a URL");
-			}
+				ress.end(rows[0].url);
+			});
 
-		}else if(parsedURL['pathname'] === '/url'){
-
-			var parsedUrl = url.parse(req.url, true);
-			var queryAsObject = parsedUrl.query;
-			var id = queryAsObject['id'];
-			if (typeof id != 'undefined') {
-
-
-				r.db('URL_Shortening').table("urls").filter({"id": id}).pluck("url").limit(1).run(conn, function(err, res)  {
-					if(err) throw err;
-					res.toArray( function(error, results) {
-						var urlData = results['0'];
-						ress.writeHead(200, {'Content-Type': 'text/plain'});
-						ress.end(urlData['url']);
-					})
-				});
-
-			}else{
-				ress.writeHead(200, {'Content-Type': 'text/plain'});
-				ress.end("Please insure you have included a ID");
-			}
-		}else{
-
-			ress.writeHead(404, {'Content-Type': 'text/plain'});
-			ress.end("File Not Found");
+		} else {
+			ress.writeHead(200, {'Content-Type': 'text/plain'});
+			ress.end("Please insure you have included a ID");
 		}
-	}).listen(80, settings.nodejs_ip);
+	} else {
+
+		ress.writeHead(404, {'Content-Type': 'text/plain'});
+		ress.end("File Not Found");
+	}
+}).listen(8081, settings.nodejs_ip);
 console.log('Server Running');
 
-});
