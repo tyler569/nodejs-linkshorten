d0748a0cb99f90826bb92832835a23239164b9ac
Tyler Philbrick <tyler569@gmail.com>
2014-08-06 10:01:25 -0400

    Whitespace and format changes

--- server.js
+++ server.js
@@ -20,19 +20,23 @@ var url = require('url');
 var mysql = require('mysql');
 
 // Pull in settings and connect to MySQL
+
 var settings = require('./settings.json');
 sql = mysql.createConnection(settings.mysql);
 
 sql.connect(function(err) {
-	if (err) throw err; // TODO : Make errors actually give useful reporting information
+	// TODO : Make errors actually give useful reporting information
+	if (err) throw err; 
 });
 
 // Create the urls table if it is not found in the database
 sql.query('CREATE TABLE IF NOT EXISTS urls (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, url VARCHAR(100));');
 
 http.createServer(function (req, res) {
+	
+	// Until we actually have a favicon, the server should not waste time trying to return it
 	if (req.url === '/favicon.ico') {
-		return;  // Until we actually have a favicon, the server should not waste time trying to return it
+		return;
 	}
 	
 	var parsedURL = url.parse(req.url, true);
@@ -41,39 +45,53 @@ http.createServer(function (req, res) {
 
 	if (parsedURL['pathname'] === '/'){
 		var queryAsObject = parsedURL.query;
+		
 		// Pull the variables out of the passed URL
 		var getURL = queryAsObject['url'];
 		var getID = queryAsObject['id'];
+		
 		// I don't want both vars in one request
-		if (typeof getURL != 'undefined' && typeof getID === 'undefined') { // New entry
+		// New entry
+		if (typeof getURL != 'undefined' && typeof getID === 'undefined') {
 			sql.query('INSERT INTO urls VALUES (id, ?);', [getURL], function(err, result){
 				if (err) throw err;
-				res.writeHead(200, {'Content-Type': 'text/plain'}) // TODO : make actual pages
+				//TODO : make actual pages
+				res.writeHead(200, {'Content-Type': 'text/plain'})
 				var idOut=result.insertId.toString();
 				res.end('Your id is: '+idOut);
-				console.log(getURL+' saved by '+remoteIP+' at id '+idOut); // Log out queries
+				// Log out queries
+				console.log(getURL+' saved by '+remoteIP+' at id '+idOut);
 			});
-		} else if (typeof getID != 'undefined' && typeof getURL === 'undefined') { // Return URL
+		}
+		// Return URL 
+		else if (typeof getID != 'undefined' && typeof getURL === 'undefined') {
 			sql.query('SELECT url FROM urls WHERE id=?;', [getID], function(err, rows){
 				if (err) throw err;
 				urlOut=rows[0].url;
+				
 				// Since this forewards automatically, add "http://" if it wasn't given
 				if (url.parse(urlOut)['protocol'] === null){
 					urlOut = 'http://'+urlOut;
 				}
 				res.writeHead(301, {'Location': urlOut});
 				res.end();
-				console.log(urlOut+' (id '+getID+') requested by '+remoteIP); // Log out queries
+				// Log out queries
+				console.log(urlOut+' (id '+getID+') requested by '+remoteIP);
+				
 				// TODO : Add a link to the page in body for browsers that don't autoredirect
 			});
-		} else { // "Home" "page"
+		}
+		// "Home" "page" 
+		else {
 			res.writeHead(200, {'Content-Type': 'text/plain'});
 			res.end('Landing page TBI here');
 		}
-	} else {
+	}
+	else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
 		res.end("File Not Found");
 	}
 }).listen(settings.nodejs_port, settings.nodejs_ip);
+
 console.log('Server Running');
 
